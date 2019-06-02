2620f34d0b7e0f5031725e75967d1008c899c104
Ilya Koy <Ilya.koy@outlook.com>
2019-06-02 08:20:07 -0400

    saving just in case

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,76 @@
 from .exceptions import *
+from random import choice
+#from exceptions import *
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ['Python','javascript','microsoft']
 
 
 def _get_random_word(list_of_words):
-    pass
+    if list_of_words == []:
+        raise InvalidListOfWordsException()
+    
+    return choice(list_of_words)
 
 
 def _mask_word(word):
-    pass
+    masked_word = ''
+    for char in word:
+        masked_word += '*'
+    
+    if masked_word == '':
+        raise InvalidWordException()
+    return masked_word
+
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    #testing with answer_word are missing
+    if answer_word == '' or masked_word == '':
+        raise InvalidWordException()
+
+    #Different lengths of answered_word or masked_word
+    if len(answer_word) != len(masked_word):
+        raise InvalidWordException()
+
+    #invalid character
+    if len(character) > 1:
+        raise InvalidGuessedLetterException()
+
+    #if Character not in word return masked_word
+    if character.lower() not in answer_word.lower():
+        return masked_word
+
+    #grab all the indexes where character may appear
+    idxs = []
+    for index,letter in enumerate(answer_word):
+        if letter.lower() == character.lower():
+            idxs.append(index)
+    
+    #update new_masked word
+    new_masked_word = ''
+    for index,char in enumerate(masked_word):
+        if index in idxs:
+            new_masked_word += character.lower()
+        elif char != '*':
+            new_masked_word += char
+        else:
+            new_masked_word += '*'
+    
+   
+    return new_masked_word
 
 
 def guess_letter(game, letter):
-    pass
+    #Test for letter
+    if letter.lower() in game['answer_word'].lower():
+        
+        game['masked_word'] = _uncover_word(game['answer_word'],game['masked_word'], letter)
+        game['previous_guesses'].append(letter).lower()
+    else:
+        game['previous_guesses'].append(letter).lower()
+        game['remaining_misses'] -= 1
+    
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
